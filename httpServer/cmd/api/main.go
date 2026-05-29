005dbd2ded63125b395d912755b910eafb819af4
berzz26 <aumtamboli2233@gmail.com>
2026-05-29 05:12:35 +0000

    init files

---
+++ httpServer/cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "health ok")
+	})
+
+	log.Print("Server listening on 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	
+}
