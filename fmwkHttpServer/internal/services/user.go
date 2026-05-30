f525fc56c9b73e7cc309647b19263397b6e7d730
berzz26 <aumtamboli2233@gmail.com>
2026-05-30 13:56:56 +0000

    feat: add user service initializing user repository

---
+++ fmwkHttpServer/internal/services/user.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"fmwkHttpServer/internal/models"
+	"fmwkHttpServer/internal/repository"
+)
+
+type UserService struct {
+	repo *repository.UserRepository
+}
+
+func NewUserService(
+	repo *repository.UserRepository,
+) *UserService {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+func (s *UserService) GetUsers() ([]model.User, error) {
+	return s.repo.GetUsers()
+}
+
+func (s *UserService) GetOneUser(id int) (*model.User, error) {
+	return s.repo.GetOneUser(id)
+}
