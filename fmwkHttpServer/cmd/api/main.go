23520770d71ac807d2aa254f6f7ed3d5f38ce619
berzz26 <aumtamboli2233@gmail.com>
2026-06-01 05:29:01 +0000

    feat: implement a make file as an entry point, updated api to handle all routes

--- fmwkHttpServer/cmd/api/main.go
+++ fmwkHttpServer/cmd/api/main.go
@@ -3,9 +3,30 @@ package main
 import (
 	"fmwkHttpServer/internal/database"
 	"fmwkHttpServer/internal/handlers"
+	"fmwkHttpServer/internal/repository"
+	"fmwkHttpServer/internal/routes"
+	"fmwkHttpServer/internal/services"
+
+	"github.com/gofiber/fiber/v2"
+
 	"log"
 )
 
 func main() {
-	
+	db := database.ConnectDB()
+
+	repo := repository.NewUserRepository(db)
+
+	userService := service.NewUserService(repo)
+
+	handler := handlers.NewUserHandler(userService)
+
+	app := fiber.New()
+
+	routes.SetupRoutes(app, handler)
+
+	log.Println("server up at 8080")
+
+	app.Listen(":8080")
+
 }
