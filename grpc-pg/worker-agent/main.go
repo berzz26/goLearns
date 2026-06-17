2d6c709f8f3229146087ec898fae2cc84c1b2077
berzz26 <aumtamboli2233@gmail.com>
2026-06-17 06:12:36 +0000

    fix: transfer hb creation inside the time loop to ensure that new values are created everytime and sent

--- grpc-pg/worker-agent/main.go
+++ grpc-pg/worker-agent/main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	pb "grpc-pg/gen/worker"
+	"math/rand/v2"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -27,22 +28,22 @@ func main() { //connect to the server
 
 	// build a req
 	id := 1
-	workerId := fmt.Sprintf("worker %d", id)
-	hb := &pb.Heartbeat{
-		WorkerId: workerId,
-		CpuUsage: 42.5,
-		RamUsage: 61.3,
-		GpuUsage: 78.1,
-	}
+	workerId := fmt.Sprintf("worker-%d", rand.Int32N(10))
 
 	// call the rpc
 	for {
+		hb := &pb.Heartbeat{
+			WorkerId: workerId,
+			CpuUsage: rand.Float64() * 10,
+			RamUsage: rand.ExpFloat64() * 10,
+			GpuUsage: 78.1,
+		}
 		resp, err := client.SendHeartbeat(
 			context.Background(),
 			hb,
 		)
-		id+=1
-		time.Sleep(5 * time.Second)
+		id += 1
+		time.Sleep(3 * time.Second)
 		if err != nil {
 			panic(err)
 		}
